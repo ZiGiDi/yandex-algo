3f1ddb70753872de1fe30a2f4cafa6ba1f448ba9
Evgeniy Zavyalov <zigidi1994@gmail.com>
2023-09-03 21:13:46 +0700

    sprint 2 final tasks: add changes after review

--- src/sprint2/review/A.java
+++ src/sprint2/review/A.java
@@ -4,29 +4,78 @@ import java.io.*;
 
 public class A {
 
+
+
+//    Гоша реализовал структуру данных Дек, максимальный размер которого определяется заданным числом. Методы push_back(x), push_front(x), pop_back(), pop_front() работали корректно. Но, если в деке было много элементов, программа работала очень долго. Дело в том, что не все операции выполнялись за O(1). Помогите Гоше! Напишите эффективную реализацию.
+//
+//Внимание: при реализации используйте кольцевой буфер.
+//
+//Формат ввода
+//В первой строке записано количество команд n — целое число, не превосходящее 100000. Во второй строке записано число m — максимальный размер дека. Он не превосходит 50000. В следующих n строках записана одна из команд:
+//
+//push_back(value) – добавить элемент в конец дека. Если в деке уже находится максимальное число элементов, вывести «error».
+//push_front(value) – добавить элемент в начало дека. Если в деке уже находится максимальное число элементов, вывести «error».
+//pop_front() – вывести первый элемент дека и удалить его. Если дек был пуст, то вывести «error».
+//pop_back() – вывести последний элемент дека и удалить его. Если дек был пуст, то вывести «error».
+//Value — целое число, по модулю не превосходящее 1000.
+//Формат вывода
+//Выведите результат выполнения каждой команды на отдельной строке. Для успешных запросов push_back(x) и push_front(x) ничего выводить не надо.
+
+    /*
+    id: https://contest.yandex.ru/contest/22781/run-report/90043266/
+
+-- ПРИНЦИП РАБОТЫ --
+Как и требовалось в задании, все реализованно на кольцевом буфере.
+Есть некий фиксированный массив в 2 указетлями на начало и конец.
+Указатель на начало при добавлении элемента движется в направление уменьшения по индексу, а конца в повышение при добавлении элемента
+Так же используется параметр количества текущих элементов в деке, чтобы не проходить весь массив в подсчете количества элементов или какой-либо другой оперрацией.
+При добавлении в начало или конец проверяется есть ли свободное место, в случае наличие оного,
+элемент добавляется и указатель сдвигается на 1 позицию. Если в начало, то вправо, если в конец то влево.
+В случаю удаление элемента указатель сдвигается в противпоположном направлении.
+т.к. у нас кольцевой буфер, то в граничных условиях указатели переходят с нулевого элемента массива в самый последний,
+или с последнего на первый и продолжают дижение на увеличение или уменьшение индекса
+
+-- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
+Т.к. у нас фиксированный массив и указатели элементов могут сдвигаться только на 1 элемент
+(в границах сдвиг переходит с одной границы на другую),
+указатели начала и конца движутся в противополжных направлениях из начальной точки,
+массив будет заполнен без пробелов, при добавлении или удалении элемента счетчик всегда реагирует и переполнение буффера невозможно.
+В любой момент времени мы точно знаем где находится верхний и нижний элемент.
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+Добавление в очередь стоит O(1), потому что добавление в фиксированный массив в определенную позицию стоит O(1).
+Извлечение из очереди стоитстоит O(1), потому что выгрузка элемента вфиксированный массив из определенной позицию стоит O(1).
+Т.к каждая опреация стоит O(1) и по всем командам мы проходим только за 1 раз,
+Итого имеем временную сложность O(n), где n - число команд
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+По условию мы задаем начальный размер буффера k.
+Массив, содержащий k элементов, занимает O(k) памяти.
+*/
+
     private static void runCommand(MyCircleDequeue queue,
                                    String command,
                                    BufferedWriter writer) throws IOException {
-        if (command.contains(" ")) {
-            try {
+        try {
+            if (command.contains(" ")) {
                 String[] s = command.split(" ");
                 if ("push_back".equals(s[0])) {
-                    queue.pushBack(s[1]);
+                    queue.pushBack(Integer.parseInt(s[1]));
                 }
                 if ("push_front".equals(s[0])) {
-                    queue.pushFront(s[1]);
+                    queue.pushFront(Integer.parseInt(s[1]));
                 }
-            } catch (FullDequeException exception) {
-                writer.write("error");
+            }
+            if ("pop_front".equals(command)) {
+                writer.write(queue.popFront().toString());
                 writer.newLine();
             }
-        }
-        if ("pop_front".equals(command)) {
-            writer.write(queue.popFront());
-            writer.newLine();
-        }
-        if ("pop_back".equals(command)) {
-            writer.write(queue.popBack());
+            if ("pop_back".equals(command)) {
+                writer.write(queue.popBack().toString());
+                writer.newLine();
+            }
+        } catch (FullDequeException | EmptyDequeException exception) {
+            writer.write("error");
             writer.newLine();
         }
     }
@@ -50,21 +99,21 @@ public class A {
 
     private static class MyCircleDequeue {
 
-        private String[] queue;
+        private Integer[] queue;
         private int head;
         private int tail;
         private int maxSize;
         private int size;
 
         public MyCircleDequeue(int queueSize) {
-            this.queue = new String[queueSize];
+            this.queue = new Integer[queueSize];
             head = 1;
             tail = 0;
             maxSize = queueSize;
             size = 0;
         }
 
-        public void pushBack(String value) throws FullDequeException {
+        public void pushBack(Integer value) throws FullDequeException {
             if (isFull()) {
                 throw new FullDequeException();
             }
@@ -73,7 +122,7 @@ public class A {
             size++;
         }
 
-        public void pushFront(String value) throws FullDequeException {
+        public void pushFront(Integer value) throws FullDequeException {
             if (isFull()) {
                 throw new FullDequeException();
             }
@@ -82,23 +131,23 @@ public class A {
             size++;
         }
 
-        public String popBack() {
+        public Integer popBack() throws EmptyDequeException {
             if (isEmpty()) {
-                return "error";
+                throw new EmptyDequeException();
             }
             tail = (tail + 1) % maxSize;
-            String value = queue[tail];
+            Integer value = queue[tail];
             queue[tail] = null;
             size--;
             return value;
         }
 
-        public String popFront() {
+        public Integer popFront() throws EmptyDequeException {
             if (isEmpty()) {
-                return "error";
+                throw new EmptyDequeException();
             }
             head = (head - 1 + maxSize) % maxSize;
-            String value = queue[head];
+            Integer value = queue[head];
             queue[head] = null;
             size--;
             return value;
@@ -114,5 +163,8 @@ public class A {
     }
 
     public static class FullDequeException extends Exception {
+    }
+
+    public static class EmptyDequeException extends Exception {
     }
 }
--- src/sprint2/review/B.java
+++ src/sprint2/review/B.java
@@ -4,24 +4,71 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class B {
 
+//Задание связано с обратной польской нотацией. Она используется для парсинга арифметических выражений. Еще её иногда называют постфиксной нотацией.
+//
+//В постфиксной нотации операнды расположены перед знаками операций.
+//
+//Пример 1:
+//3 4 +
+//означает 3 + 4 и равно 7
+//
+//Пример 2:
+//12 5 /
+//Так как деление целочисленное, то в результате получим 2.
+//
+//Пример 3:
+//10 2 4 * -
+//означает 10 - 2 * 4 и равно 2
+
+        /*
+    id: https://contest.yandex.ru/contest/22781/run-report/90042935/
+
+-- ПРИНЦИП РАБОТЫ --
+На вход подается строка состоящая из чисел и оперантов.
+Если элемент строки является числом, то он помещается в стэк.
+Если оперант, то из стэка достаются 2 элемента и производится математическая операция,
+затем результат операции помещается в стэк. Нужно учитывать, что порядок чисел в математических операциях
+с обратной польской натацией идет слева направо в 2 последних числах перед операндом.
+
+-- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
+Т.к. все числа мы ложем в стэк  итерируясь от начала входного списка элементов,
+дойдя до операнда мы в любой момент времени можем вытащить только те числа, которые были перед операндом (LIFO),
+или результат предыдущего операнда, т.к. после математической операции результат помещается в Стэк.
+Порядок элементов гарантирован из-за стэка, и порядок операций гарантирован, т.к. мы итерируемся последовательно.
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+Добавление в стэк стоит O(1), потому что в основании хранилища элементов используемого стэка лежит массив и указатель на индекс последнего элемента,
+ а добавить элемент в свободный слот массива O(1). Но если массив полный, то стоимсоть операции будет О(n) где n - количество элементов в массиве,
+ т.к. придется расширять массив и копировать все элементы из старого в новый
+
+Извлечение из стэка стоит O(1), потому что в основании хранилища элементов используемого стэка лежит массив и указатель на индекс последнего элемента,
+А получение элемента из массива по индексу O(1)
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+В итерации по элемнтам используется стэк, в основании которого лежит массив с указателем,
+куда будут помещаться только числа. В худшем случае все элементы входой строки будут являться символами,
+и тогда они попадут в стэк и займут память O(n), где n - количество элементов
+
+Итого O(n);
+*/
+
     public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            String line = reader.readLine();
-            int chaosNumber = calculate(line);
+            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+            int chaosNumber = calculate(tokenizer);
             System.out.println(chaosNumber);
         }
     }
 
-    private static int calculate(String line) {
+    private static int calculate(StringTokenizer tokenizer) {
         Stack<Integer> stackForNumbers = new Stack<>();
-        String[] symbols = line.split(" ");
-        for (String symbol : symbols) {
-            if (symbol.matches("[-+]?\\d+")) {
-                stackForNumbers.push(Integer.parseInt(symbol));
-            } else if ("+".equals(symbol)) {
+        while (tokenizer.hasMoreElements()) {
+            String symbol = tokenizer.nextToken();
+            if ("+".equals(symbol)) {
                 int secondElem = stackForNumbers.pop();
                 int firstElem = stackForNumbers.pop();
                 stackForNumbers.push(firstElem + secondElem);
@@ -37,6 +84,8 @@ public class B {
                 int secondElem = stackForNumbers.pop();
                 int firstElem = stackForNumbers.pop();
                 stackForNumbers.push(Math.floorDiv(firstElem, secondElem));
+            } else {
+                stackForNumbers.push(Integer.parseInt(symbol));
             }
         }
         return stackForNumbers.pop();
