a521c3d6c525fc2d6204ce2a07bbf6c9ac789ccd
Evgeniy Zavyalov <zigidi1994@gmail.com>
2023-09-16 19:05:16 +0700

    sprint 4 tasks H: add solution for task Strange Comparison

--- src/sprint4/H.java
+++ src/sprint4/H.java
@@ -4,6 +4,17 @@ import java.io.*;
 import java.util.*;
 
 public class H {
+    /*
+    Жители Алгосского архипелага придумали новый способ сравнения строк. Две строки считаются равными,
+    если символы одной из них можно заменить на символы другой так, что первая строка станет точной копией второй строки.
+    При этом необходимо соблюдение двух условий:
+
+    Порядок вхождения символов должен быть сохранён.
+    Одинаковым символам первой строки должны соответствовать одинаковые символы второй строки. Разным символам —– разные.
+    Например, если строка s = «abacaba», то ей будет равна строка t = «xhxixhx», так как все вхождения «a» заменены на «x»,
+    «b» –— на «h», а «c» –— на «i». Если же первая строка s=«abc», а вторая t=«aaa», то строки уже не будут равны, так как
+    разные буквы первой строки соответствуют одинаковым буквам второй.
+     */
 
     public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -21,27 +32,20 @@ public class H {
             return false;
         }
 
-        Map<Character, Integer> mapA = new LinkedHashMap<>();
-        Map<Character, Integer> mapB = new LinkedHashMap<>();
+        Map<Character, Character> dictionary = new HashMap<>();
         char[] charArrayA = stringA.toCharArray();
         char[] charArrayB = stringB.toCharArray();
         for (int i = 0; i < charArrayA.length; i++) {
             char charA = charArrayA[i];
             char charB = charArrayB[i];
-            if (mapA.containsKey(charA) && mapB.containsKey(charB)) {
-                if (!mapA.get(charA).equals(mapB.get(charB))) {
-                    return false;
-                } else {
-                    mapA.put(charA, i);
-                    mapB.put(charB, i);
-                }
-            } else if (!mapA.containsKey(charA) && !mapB.containsKey(charB)) {
-                mapA.put(charA, i);
-                mapB.put(charB, i);
-            } else {
+            boolean containsChar = dictionary.containsKey(charA);
+            if (containsChar && !dictionary.get(charA).equals(charB)) {
                 return false;
+            } else if (!containsChar) {
+                dictionary.put(charA, charB);
             }
         }
-        return true;
+        HashSet<Character> characters = new HashSet<>(dictionary.values());
+        return dictionary.size() == characters.size();
     }
 }
