c76c44a0ba0e13f5392ddd1ad129b8d5e8f2425c
Evgeniy Zavyalov <zigidi1994@gmail.com>
2023-11-12 14:01:32 +0700

    sprint 6 final tasks A & B: add description

--- src/sprint6/review/B.java
+++ src/sprint6/review/B.java
@@ -5,11 +5,33 @@ import java.util.*;
 
 public class B {
 
-    private static final int WHITE = 0;
-    private static final int GREY = 1;
-    private static final int BLACK = 2;
+            /*
+    id: https://contest.yandex.ru/contest/25070/run-report/96906967/
 
-    private static List<Integer> colors;
+-- ПРИНЦИП РАБОТЫ --
+    Сам граф храним в виде списка смежных вершин. Ребра R разворачиваем.
+    А для поиска циклов будем использовать обход в глубину.То есть из каждой вершины, в которую мы ещё ни разу не приходили,
+запустим поиск в глубину, который при входе в вершину будет красить её в серый цвет, а при выходе из нее — в чёрный.
+И, если алгоритм пытается пойти в серую вершину, то это означает, что цикл найден.
+
+https://neerc.ifmo.ru/wiki/index.php?title=%D0%98%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5_%D0%BE%D0%B1%D1%85%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%B3%D0%BB%D1%83%D0%B1%D0%B8%D0%BD%D1%83_%D0%B4%D0%BB%D1%8F_%D0%BF%D0%BE%D0%B8%D1%81%D0%BA%D0%B0_%D1%86%D0%B8%D0%BA%D0%BB%D0%B0
+
+-- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
+    Т.к. в основании обхода мы используем обход в глубину DFS, то при обходе, если мы натыкаемся на вершину, которую уже окрасили в серый цвет,
+т.е. уровнем выше, то граф циккличен.
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+   O(V + E), где V - количество вершин, E - количество ребер. Обусловлено логикой алгоритма DFS
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+    Память для списка смежности: O(E+V), где E - количество ребер, V - количество вершин.
+    Отдельно тратим память для хранения цветов вершин O(V).
+    Так же для DFS используется не рекурсивный вызов, а стэк в который кладутся и достаются вершины - O(V)
+
+    Итого, при удалении констант: O(E+V)
+     */
+
+    private static Color[] colors;
     private static List<List<Integer>> adjacencyList;
 
     public static void main(String[] args) throws IOException {
@@ -58,52 +80,56 @@ public class B {
     }
 
     private static boolean checkCyclicGraph(int numberOfVertexes) {
-        colors = initializeListWithDefaultValue(numberOfVertexes, WHITE);
+        colors = initializeColors(numberOfVertexes);
 
         for (int i = 0; i < numberOfVertexes; i++) {
-            if (colors.get(i) != WHITE) {
+            if (colors[i] != Color.WHITE) {
                 continue;
             }
-            if (BFS(i)) {
+            if (DFS(i)) {
                 return true;
             }
         }
         return false;
     }
 
-    private static List<Integer> initializeListWithDefaultValue(int numberOfVertexes, int defaultValue) {
-        List<Integer> colors = new ArrayList<>();
-        for (int i = 0; i < numberOfVertexes; i++) {
-            colors.add(defaultValue);
-        }
+    private static Color[] initializeColors(int numberOfVertexes) {
+        Color[] colors = new Color[numberOfVertexes];
+        Arrays.fill(colors, Color.WHITE);
         return colors;
     }
 
-    private static boolean BFS(int s) {
+    private static boolean DFS(int s) {
         // Создадим очередь вершин и положим туда стартовую вершину.
         Stack<Integer> planned = new Stack<>();
         planned.add(s);
         while (!planned.isEmpty()) {
             int u = planned.pop();// Возьмём вершину из очереди.
-            if (colors.get(u) == WHITE) {
-                colors.set(u, 1);
+            if (colors[u] == Color.WHITE) {
+                colors[u] = Color.GREY;
                 planned.add(u);
 
                 for (int v : adjacencyList.get(u)) { // adjList - список смежности графа.
-                    if (colors.get(v) == WHITE) { // Серые и чёрные вершины уже
+                    if (colors[v] == Color.WHITE) { // Серые и чёрные вершины уже
                         // либо в очереди, либо обработаны.
                         planned.add(v); // Запланируем посещение вершины.
                     }
-                    if (colors.get(v) == GREY) {
+                    if (colors[v] == Color.GREY) {
                         return true; //Если наталкиваемся уже на серую вершину, значит граф цикличный
                     }
                 }
                 continue;
             }
-            if (colors.get(u) == GREY) {
-                colors.set(u, BLACK); // В конце красим обработанные вершины в черный цвет
+            if (colors[u] == Color.GREY) {
+                colors[u] = Color.BLACK; // В конце красим обработанные вершины в черный цвет
             }
         }
         return false;
+    }
+
+    public enum Color {
+        WHITE,
+        GREY,
+        BLACK
     }
 }
