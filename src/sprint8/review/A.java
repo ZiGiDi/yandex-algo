436d00e8aa25fbcac4c53799645c5f8392f2a6d6
Evgeniy Zavyalov <zigidi1994@gmail.com>
2023-12-04 22:50:07 +0700

    sprint 8 final task A: add solution for task Packed Prefix

--- src/sprint8/review/A.java
+++ src/sprint8/review/A.java
@@ -1,8 +1,7 @@
 package sprint8.review;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class A {
     public static void main(String[] args) throws IOException {
@@ -48,28 +47,27 @@ public class A {
     }
 
     private static String unpackLine(String line) {
-
-        while (line.contains("[")) {
-
-            int indexOfBegin = line.lastIndexOf("[");
-            int indexOfEnd = line.indexOf("]", indexOfBegin);
-            String packedWord = line.substring(indexOfBegin + 1, indexOfEnd);
-
-            char[] charArray = line.substring(0, indexOfBegin)
-                    .toCharArray();
-            int i = indexOfBegin - 1;
-            while (i > 0 && Character.isDigit(charArray[i - 1])) {
-                i--;
+        Stack<String> stack = new Stack<>();
+        for (char c : line.toCharArray()) {
+            if (c == ']') {
+                List<String> packedWord = new ArrayList<>();
+                String stackChar = stack.pop();
+                while (!stackChar.equals("[")) {
+                    packedWord.add(stackChar);
+                    stackChar = stack.pop();
+                }
+                Collections.reverse(packedWord);
+                String word = String.join("", packedWord);
+                int count = Integer.parseInt(stack.pop());
+                stack.push(word.repeat(count));
+            } else {
+                stack.push(Character.toString(c));
             }
-            int increment = Integer.parseInt(line.substring(i, indexOfBegin));
-
-            String unpackedPart = packedWord.repeat(increment);
-            String regex = increment + line.substring(indexOfBegin, indexOfEnd + 1);
-            line = line.replace(regex, unpackedPart);
         }
-
-        return line;
+        StringBuilder result = new StringBuilder();
+        for (String str : stack) {
+            result.append(str);
+        }
+        return result.toString();
     }
-
-
 }
